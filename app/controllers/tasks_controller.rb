9c3f4357767dbea371fd394dfc5f3fe03e5369f3
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-09 09:56:11 +0900

    taskを保存できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,16 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク: #{task.title}を登録しました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :priority, :deadline, :state)
+  end
 end

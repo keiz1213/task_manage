90255d724a52074feb8637006e0d08c693572880
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-11 09:39:15 +0900

    factroyを自動で作るためgeneratorの設定を変更

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module TaskManage
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false

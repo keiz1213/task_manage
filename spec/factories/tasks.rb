ce564ca08c3f5cdd43a1b41083b110e71dbe9fd2
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-11 11:09:20 +0900

    バリデーションのテスト追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,5 +5,26 @@ FactoryBot.define do
     priority { 'low' }
     deadline { Time.current.tomorrow }
     state { 'not_started' }
+
+    trait :non_title do
+    end
+
+    trait :over_30_length do
+    end
+
+    trait :non_description do
+    end
+
+    trait :non_priority do
+    end
+
+    trait :invalid_priority do
+    end
+
+    trait :non_deadline do
+    end
+
+    trait :deadline_before_current do
+    end
   end
 end

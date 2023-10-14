b30184882254bd1af34dcad38b50c6b7846b7514
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-14 13:20:26 +0900

    タスク検索のexample追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -29,10 +29,13 @@ RSpec.describe "Tasks" do
       day_before_yesterday_task_title = create(:task, :day_before_yesterday_task).title
       visit root_path
 
-      titles = page.text.scan(/(#{today_task_title}|#{yesterday_task_title}|#{day_before_yesterday_task_title})/)
-      expect(titles[0][0]).to eq today_task_title
-      expect(titles[1][0]).to eq yesterday_task_title
-      expect(titles[2][0]).to eq day_before_yesterday_task_title
+      within(:test, 'task-list') do
+        task_titles = all(:test, 'task-title')
+        expect(task_titles.count).to be 3
+        expect(task_titles[0].text).to eq today_task_title
+        expect(task_titles[1].text).to eq yesterday_task_title
+        expect(task_titles[2].text).to eq day_before_yesterday_task_title
+      end
     end
 
     it 'タスクの詳細' do
@@ -91,11 +94,67 @@ RSpec.describe "Tasks" do
 
       # 設定ファイル: spec/support/wait_for_css
       # 参考: https://qiita.com/johnslith/items/09bb0e5257e06a4bd948
-      wait_for_css_appear('.card-header') do
-        titles = page.text.scan(/(#{due_tomorrow_task_title}|#{due_day_after_tomorrow_task_title}|#{due_two_days_after_tomorrow_task_title})/)
-        expect(titles[0][0]).to eq due_tomorrow_task_title
-        expect(titles[1][0]).to eq due_day_after_tomorrow_task_title
-        expect(titles[2][0]).to eq due_two_days_after_tomorrow_task_title
+      wait_for_css_appear('.card') do
+        within(:test, 'task-list') do
+          task_titles = all(:test, 'task-title')
+          expect(task_titles.count).to be 3
+          expect(task_titles[0].text).to eq due_tomorrow_task_title
+          expect(task_titles[1].text).to eq due_day_after_tomorrow_task_title
+          expect(task_titles[2].text).to eq due_two_days_after_tomorrow_task_title
+        end
+      end
+    end
+  end
+
+  describe '検索' do
+    let!(:task1) { create(:task, title: 'りんご君', description: 'バナナ', deadline: Time.current.since(5.days))}
+    let!(:task2) { create(:task, title: 'スイカ', description: '桃太郎', deadline: Time.current.since(3.days))}
+    let!(:task3) { create(:task, title: 'メロン', description: '甘いいちご', deadline: Time.current.since(2.days))}
+    let!(:task4) { create(:task, title: 'りんごちゃん', description: 'すっぱいいちご', deadline: Time.current.since(4.days))}
+    let!(:task5) { create(:task, title: 'パイナップル', description: 'いちご', deadline: Time.current.since(1.day))}
+
+    it 'タイトルからキーワード検索できる' do
+      visit root_path
+      fill_in 'キーワード', with: 'りんご'
+      click_button '検索する'
+      wait_for_css_appear('.card') do
+        within(:test, 'task-list') do
+          task_titles = all(:test, 'task-title')
+          expect(task_titles.count).to be 2
+          expect(task_titles[0].text).to match /りんご/
+          expect(task_titles[1].text).to match /りんご/
+        end
+      end
+    end
+
+    it '説明からキーワード検索できる' do
+      visit root_path
+      fill_in 'キーワード', with: '桃'
+      click_button '検索する'
+      wait_for_css_appear('.card') do
+        task_titles = []
+        within(:test, 'task-list') do
+          task_titles = all(:test, 'task-title')
+          expect(task_titles.count).to be 1
+        end
+        click_link task_titles[0].text
+        expect(page).to have_content('桃太郎')
+      end
+    end
+
+    it '検索結果を締切が近い順にソートできる' do
+      visit root_path
+      fill_in 'キーワード', with: 'いちご'
+      click_button '検索する'
+      click_link '締切が近い順'
+      wait_for_css_appear('.card') do
+        within(:test, 'task-list') do
+          task_titles = all(:test, 'task-title')
+          expect(task_titles.count).to be 3
+          expect(task_titles[0].text).to eq task5.title
+          expect(task_titles[1].text).to eq task3.title
+          expect(task_titles[2].text).to eq task4.title
+        end
       end
     end
   end

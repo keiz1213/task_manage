b8ba0ce777d302e667da705ca4b603416d36b0cb
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-16 10:58:44 +0900

    タスクのステータス更新ボタンにデザインをあてた

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,4 +11,17 @@ module TasksHelper
     end
     tag.span(class: classes) { '重要度: ' + t("activerecord.attributes.task.priorities.#{task.priority}") }
   end
+
+  def submit_state(form, task)
+    classes = %w[btn]
+    case task.state
+    when 'not_started'
+      classes << 'btn-outline-secondary'
+    when 'in_progress'
+      classes << 'btn-outline-primary'
+    when 'done'
+      classes << 'btn-outline-success'
+    end
+    form.submit t("activerecord.attributes.task.states.#{task.state}"), data: { test: 'update-state'}, class: classes
+  end
 end

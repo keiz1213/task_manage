faf917804c95933258d8f6ce9ad18a83d63980f1
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-16 20:16:31 +0900

    ステータスで絞り込む時に、現在絞り込んでいる条件が分かるようにした

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -24,4 +24,18 @@ module TasksHelper
     end
     form.submit t("activerecord.attributes.task.states.#{task.state}"), data: { test: 'update-state'}, class: classes
   end
+
+  def link_to_search_by_state(text, state, search_form)
+    classes = %w[btn]
+    classes << 'active' if search_form.state == state
+    case state
+    when 'not_started'
+      classes << 'btn-outline-secondary'
+    when 'in_progress'
+      classes << 'btn-outline-primary'
+    when 'done'
+      classes << 'btn-outline-success'
+    end
+    link_to text, tasks_path(search: { keyword: search_form.keyword, sort_by: search_form.sort_by, state: state }), method: 'get', class: classes
+  end
 end

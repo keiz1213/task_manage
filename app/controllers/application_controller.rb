008a440f1090105b910c2eb07526a91c46f03630
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-16 09:05:46 +0900

    application_controllerから検索用インスタンス変数を渡すようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search_form
+  def set_search_form
+    @search_form = TaskSearchForm.new(search_params)
+  end
+
+  private
+
+  def search_params
+    params[:search].present? ? params.require(:search).permit(:keyword, :sort_by, :state) : {}
+  end
 end

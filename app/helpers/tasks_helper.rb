34cdd70ca2873be0b3ccdbcb79d71e7fe45c68eb
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-16 10:36:54 +0900

    タスクに重要度を表すバッジ追加

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
 module TasksHelper
+  def priority_badge(task)
+    classes = %w[badge rounded-pill my-auto py-2]
+    case task.priority
+    when 'low'
+      classes << 'text-bg-primary'
+    when 'mid'
+      classes << 'text-bg-warning'
+    when 'high'
+      classes << 'text-bg-danger'
+    end
+    tag.span(class: classes) { '重要度: ' + t("activerecord.attributes.task.priorities.#{task.priority}") }
+  end
 end

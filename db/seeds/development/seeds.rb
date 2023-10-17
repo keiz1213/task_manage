e9f13ab40e97f2ce40fd2be5eb05d4ec9dc905e3
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 13:54:05 +0900

    seedsを実行環境で動的に変更できるようにした

---
+++ db/seeds/development/seeds.rb
@@ -0,0 +1,32 @@
+# 重要度を指定してその重要度のタスクを9個つくる
+def create_tasks(user:, priority:)
+  state = ''
+  3.times do |i1|
+    case i1
+    when 0
+      state = 'not_started'
+    when 1
+      state = 'in_progress'
+    when 2
+      state = 'done'
+    end
+    3.times do |i2|
+      user.tasks.create(
+        title: Faker::Book.unique.title,
+        description: Faker::Lorem.sentence,
+        priority: priority,
+        deadline: Time.mktime(2100, (1 + i2), 2, 3, 4),
+        state: state
+      )
+    end
+  end
+end
+
+master_user = User.find_or_create_by!(email: 'master@example.com') do |user|
+  user.name = 'master'
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+create_tasks(user: master_user, priority: 'low')
+create_tasks(user: master_user, priority: 'mid')
+create_tasks(user: master_user, priority: 'high')

b88204018bbd925e77c7f2f749fcbfa25887b0e5
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 19:37:28 +0900

    sessions#create追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      reset_session
+      login(user)
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'パスワードまたはメールアドレスが間違っています'
+      render :new, status: :unprocessable_entity
+    end
+  end
 end

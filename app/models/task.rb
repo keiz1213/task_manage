2772bbdfc838aadacad5bcce613bfb264e84468b
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 10:37:55 +0900

    Taskにbelogs_to追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { maximum: 30 }
   validates :priority, presence: true
   validates :deadline, presence: true

8d771737ddcc34f8841cc17d12cfd765f9f6fc6b
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 17:41:32 +0900

    sessions_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
       patch :update_state
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end

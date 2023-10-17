c1a3d589ee232efbabe8719675f409ff539a1af6
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 10:35:39 +0900

    usersのname,email,password_digestにnot_null制約を付けた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_010928) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_013342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,11 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_010928) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

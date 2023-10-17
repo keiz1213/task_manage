00a0b0dc7dd1bbaebc1b769d8d3ae0c18826c145
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-17 13:58:12 +0900

    userのhas_manyメソッドにdependentオプション追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     name { 'test-user' }
-    sequence(:email) { |n| "test#{n}@example.com"}
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { '12345678' }
     password_confirmation { '12345678' }
 
     trait :master_user do
       name { 'master' }
-      email { 'master@example.com'}
+      email { 'master@example.com' }
     end
   end
 end

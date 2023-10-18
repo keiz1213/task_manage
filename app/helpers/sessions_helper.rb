0c5067c965c2f2d269fa280299b6d3a25e9140e4
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-18 11:14:06 +0900

    current_userを永続セッションに対応させた

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,15 +3,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent.encrypted[:remember_token] = user.remember_token
+  end
+
   def logout
     reset_session
     @current_user = nil
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      user && user.authenticated?(cookies.encrypted[:remember_token])
+      log_in(user)
+      @current_user = user
   end
 
   def logged_in?

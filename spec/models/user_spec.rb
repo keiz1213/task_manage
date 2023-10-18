b5a623bd711c95402f6602ec81aafe339e2b1ff8
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-19 01:27:59 +0900

    Userのモデルスペック追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'クラスメソッド' do
+    describe 'digest' do
+      it '文字列をハッシュ化する' do
+        digest = User.digest('foo')
+        expect(digest).to be_a BCrypt::Password
+      end
+    end
+
+    describe 'new_token' do
+      it 'ランダムな文字列を返す' do
+        token = User.new_token
+        expect(token).not_to be_nil
+        expect(token).to be_a String
+      end
+    end
+  end
+
+  describe 'インスタンスメソッド' do
+    describe 'remember' do
+      it 'インスタンスのremember_digest属性が更新される' do
+        user = create(:user)
+        expect(user.remember_digest).to be_nil
+        first_digest = user.remember
+        expect(user.remember_digest).to eq first_digest
+        second_digest = user.remember
+        expect(user.remember_digest).to eq second_digest
+      end
+    end
+
+    describe 'session_token' do
+      context 'インスタンスにremember_digest属性が設定されている時' do
+        it 'インスタンスのremember_digestの値を返す' do
+          user = create(:user)
+          remember_digest = user.remember
+          expect(user.session_token).to eq remember_digest
+        end
+      end
+
+      context 'インスタンスにremember_digest属性が設定されていない時' do
+        it 'インスタンスにremember_digestを設定し、その値を返す' do
+          user = create(:user)
+          expect(user.remember_digest).to be_nil
+          remember_digest = user.session_token
+          expect(user.remember_digest).to eq remember_digest
+        end
+      end
+    end
+
+    describe 'authenticated?' do
+      it 'remember_tokenの検証が成功するとtrueを返す' do
+        user = create(:user)
+        user.remember
+        expect(user.authenticated?(user.remember_token)).to be_truthy
+      end
+
+      it 'remember_tokenの検証が失敗するとfalseを返す' do
+        user = create(:user)
+        user.remember
+        expect(user.authenticated?('foo')).to be_falsey
+      end
+    end
+
+    describe 'forget' do
+      it 'インスタンスのremember_digest属性をnilに更新する' do
+        user = create(:user)
+        user.remember
+        expect(user.remember_digest).not_to be_nil
+        user.forget
+        expect(user.remember_digest).to be_nil
+      end
+    end
+  end
 end

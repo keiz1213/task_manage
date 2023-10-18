12c09bbf6af7371c634e7a9a3b72d6f2d16e0c2c
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-19 00:20:43 +0900

    task_search_form_specをユーザーモデルに対応させた

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,32 +3,63 @@ FactoryBot.define do
     title { 'test title' }
     description { 'test description' }
     priority { 'low' }
-    deadline { Time.mktime(2100, 1, 2, 3, 4) }
+    sequence(:deadline) { |n| n.weeks.from_now }
     state { 'not_started' }
+    association :user
 
     trait :yesterday_task do
       title { 'yesterday_task' }
-      created_at { Time.current.ago(1.day) }
+      created_at { 1.day.ago }
     end
 
     trait :day_before_yesterday_task do
       title { 'day_before_yesterday_task' }
-      created_at { Time.current.ago(2.days) }
+      created_at { 2.days.ago }
     end
 
     trait :due_tomorrow_task do
       title { 'due_after_1_day_task' }
-      deadline { Time.current.since(1.day) }
+      deadline { 1.day.from_now }
     end
 
     trait :due_day_after_tomorrow_task do
       title { 'due_after_2_day_task' }
-      deadline { Time.current.since(2.days) }
+      deadline { 2.days.from_now }
     end
 
     trait :due_two_days_after_tomorrow_task do
       title { 'due_after_3_day_task' }
-      deadline { Time.current.since(3.days) }
+      deadline { 3.days.from_now }
+    end
+
+    trait :low_priority_task do
+      title { 'low_priority_task' }
+      priority { 'low' }
+    end
+
+    trait :mid_priority_task do
+      title { 'mid_priority_task' }
+      priority { 'mid' }
+    end
+
+    trait :high_priority_task do
+      title { 'high_priority_task' }
+      priority { 'high' }
+    end
+
+    trait :not_started_task do
+      title { 'not_started_task' }
+      state { 'not_started' }
+    end
+
+    trait :in_progress_task do
+      title { 'in_progress_task' }
+      state { 'in_progress' }
+    end
+
+    trait :done_task do
+      title { 'done_task' }
+      state { 'done' }
     end
   end
 end
--- spec/models/task_search_form_spec.rb
+++ spec/models/task_search_form_spec.rb
@@ -1,63 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe TaskSearchForm do
-  describe 'ソート' do
-    it 'デフォルトで作成日の降順にソート' do
-      task1 = create(:task)
-      task2 = create(:task, :yesterday_task)
-      task3 = create(:task, :day_before_yesterday_task)
+  describe 'ユーザーのタスクをソートする' do
+    it 'デフォルトで作成日の新しい順にソート' do
+      user = create(:user, :with_tasks_for_3_days)
       search_form = build(:task_search_form)
+      tasks_order_by_created_at = user.tasks.recent
 
-      expect(search_form.sort_task[0]).to eq task1
-      expect(search_form.sort_task[1]).to eq task2
-      expect(search_form.sort_task[2]).to eq task3
+      expect(tasks_order_by_created_at.count).to be 3
+      expect(search_form.sort_task(user)[0]).to eq tasks_order_by_created_at[0]
+      expect(search_form.sort_task(user)[1]).to eq tasks_order_by_created_at[1]
+      expect(search_form.sort_task(user)[2]).to eq tasks_order_by_created_at[2]
     end
 
     it '締め切りに近い順にソートできる' do
-      task1 = create(:task, :due_tomorrow_task)
-      task2 = create(:task, :due_two_days_after_tomorrow_task)
-      task3 = create(:task, :due_day_after_tomorrow_task)
-      search_form = build(:task_search_form, :sort_by_deadline)
+      user = create(:user, :with_tasks_separate_due_dates)
+      search_form = build(:task_search_form, sort_by: 'deadline')
+      tasks_order_by_deadline = user.tasks.deadline
 
-      expect(search_form.sort_task[0]).to eq task1
-      expect(search_form.sort_task[1]).to eq task3
-      expect(search_form.sort_task[2]).to eq task2
+      expect(tasks_order_by_deadline.count).to be 3
+      expect(search_form.sort_task(user)[0]).to eq tasks_order_by_deadline[0]
+      expect(search_form.sort_task(user)[1]).to eq tasks_order_by_deadline[1]
+      expect(search_form.sort_task(user)[2]).to eq tasks_order_by_deadline[2]
     end
 
     it '優先度が高い順にソートできる' do
-      task1 = create(:task, title: '青りんご', priority: 'mid')
-      task2 = create(:task, title: 'スイカ', priority: 'low')
-      task3 = create(:task, title: 'メロン', priority: 'high')
+      user = create(:user, :with_tasks_separate_priority)
+      search_form = build(:task_search_form, sort_by: 'high')
+      tasks_order_by_high_priority = user.tasks.high_priority_first
 
-      search_form = build(:task_search_form, :sort_by_priority_high)
-      expect(search_form.sort_task[0]).to eq task3
-      expect(search_form.sort_task[1]).to eq task1
-      expect(search_form.sort_task[2]).to eq task2
+      expect(tasks_order_by_high_priority.count).to be 3
+      expect(search_form.sort_task(user)[0]).to eq tasks_order_by_high_priority[0]
+      expect(search_form.sort_task(user)[1]).to eq tasks_order_by_high_priority[1]
+      expect(search_form.sort_task(user)[2]).to eq tasks_order_by_high_priority[2]
     end
 
     it '優先度が低い順にソートできる' do
-      task1 = create(:task, title: '青りんご', priority: 'mid')
-      task2 = create(:task, title: 'スイカ', priority: 'low')
-      task3 = create(:task, title: 'メロン', priority: 'high')
+      user = create(:user, :with_tasks_separate_priority)
+      search_form = build(:task_search_form, sort_by: 'low')
+      tasks_order_by_low_priority = user.tasks.low_priority_first
 
-      search_form = build(:task_search_form, :sort_by_priority_low)
-      expect(search_form.sort_task[0]).to eq task2
-      expect(search_form.sort_task[1]).to eq task1
-      expect(search_form.sort_task[2]).to eq task3
+      expect(tasks_order_by_low_priority.count).to be 3
+      expect(search_form.sort_task(user)[0]).to eq tasks_order_by_low_priority[0]
+      expect(search_form.sort_task(user)[1]).to eq tasks_order_by_low_priority[1]
+      expect(search_form.sort_task(user)[2]).to eq tasks_order_by_low_priority[2]
     end
   end
 
-  describe 'サーチ' do
+  describe 'ユーザーのタスクを検索する' do
     it 'タイトルからキーワード検索できる' do
-      create(:task, title: '青りんご', description: 'バナナ')
-      create(:task, title: 'スイカ', description: '桃太郎')
-      create(:task, title: 'メロン', description: 'いちごみるく')
-      create(:task, title: 'りんごちゃん', description: 'いちご摘み')
-      create(:task, title: 'パイナップル', description: 'いちご')
+      user = create(:user, :with_tasks_separate_title)
+      search_form = build(:task_search_form, keyword: 'りんご')
+      result = search_form.search(user)
 
-      task_search_form = build(:task_search_form, keyword: 'りんご')
-      result = task_search_form.search
-
+      expect(user.tasks.count).to be 5
       expect(result.count).to be 2
       result.each do |task|
         expect(task.title).to match(/りんご/)
@@ -65,15 +61,11 @@ RSpec.describe TaskSearchForm do
     end
 
     it '説明からキーワード検索できる' do
-      create(:task, title: '青りんご', description: 'バナナ')
-      create(:task, title: 'スイカ', description: '桃太郎')
-      create(:task, title: 'メロン', description: 'いちごみるく')
-      create(:task, title: 'りんごちゃん', description: 'いちご摘み')
-      create(:task, title: 'パイナップル', description: 'いちご')
+      user = create(:user, :with_tasks_separate_title)
+      search_form = build(:task_search_form, keyword: 'いちご')
+      result = search_form.search(user)
 
-      task_search_form = build(:task_search_form, keyword: 'いちご')
-      result = task_search_form.search
-
+      expect(user.tasks.count).to be 5
       expect(result.count).to be 3
       result.each do |task|
         expect(task.description).to match(/いちご/)
@@ -81,108 +73,110 @@ RSpec.describe TaskSearchForm do
     end
 
     it 'ステータスで検索できる' do
-      create(:task, title: '青りんご', state: 'not_started')
-      create(:task, title: 'スイカ', state: 'not_started')
-      create(:task, title: 'メロン', state: 'in_progress')
-      create(:task, title: 'りんごちゃん', state: 'in_progress')
-      create(:task, title: 'パイナップル', state: 'done')
+      user = create(:user, :with_tasks_separate_state)
+      search_form = build(:task_search_form, state: 'in_progress')
+      result = search_form.search(user)
 
-      task_search_form = build(:task_search_form, state: 'in_progress')
-      result = task_search_form.search
-      expect(result.count).to be 2
+      expect(user.tasks.count).to be 3
+      expect(result.count).to be 1
       result.each do |task|
         expect(task.state).to eq 'in_progress'
       end
     end
 
-    it 'キーワード検索の結果を締切が近い順にソートできる' do
-      create(:task, title: '青りんご', description: 'バナナ', deadline: Time.current.since(5.days))
-      create(:task, title: 'スイカ', description: '桃太郎', deadline: Time.current.since(3.days))
-      create(:task, title: 'メロン', description: 'いちごみるく', deadline: Time.current.since(2.days))
-      create(:task, title: 'りんごちゃん', description: 'いちご摘み', deadline: Time.current.since(4.days))
-      create(:task, title: 'パイナップル', description: 'いちご', deadline: Time.current.since(1.day))
+    describe '検索結果のソート' do
+      let(:user) { create(:user) }
 
-      task_search_form = build(:task_search_form, keyword: 'いちご', sort_by: 'deadline')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq 'パイナップル'
-      expect(result[1].title).to eq 'メロン'
-      expect(result[2].title).to eq 'りんごちゃん'
-    end
-
-    it 'キーワード検索の結果を優先順位が高い順にソートできる' do
-      create(:task, title: '青りんご', priority: 'low')
-      create(:task, title: 'スイカ', priority: 'mid')
-      create(:task, title: '毒りんご', priority: 'high')
-      create(:task, title: 'りんごちゃん', priority: 'mid')
-      create(:task, title: 'パイナップル', priority: 'high')
-
-      task_search_form = build(:task_search_form, keyword: 'りんご', sort_by: 'high')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq '毒りんご'
-      expect(result[1].title).to eq 'りんごちゃん'
-      expect(result[2].title).to eq '青りんご'
-    end
-
-    it 'キーワード検索の結果を優先順位が低い順にソートできる' do
-      create(:task, title: '青りんご', priority: 'low')
-      create(:task, title: 'スイカ', priority: 'mid')
-      create(:task, title: '毒りんご', priority: 'high')
-      create(:task, title: 'りんごちゃん', priority: 'mid')
-      create(:task, title: 'パイナップル', priority: 'high')
-
-      task_search_form = build(:task_search_form, keyword: 'りんご', sort_by: 'low')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq '青りんご'
-      expect(result[1].title).to eq 'りんごちゃん'
-      expect(result[2].title).to eq '毒りんご'
-    end
-
-    it 'ステータス検索の結果を締切が近い順にソートできる' do
-      create(:task, title: '青りんご', state: 'not_started', deadline: Time.current.since(5.days))
-      create(:task, title: 'スイカ', state: 'not_started', deadline: Time.current.since(3.days))
-      create(:task, title: 'メロン', state: 'not_started', deadline: Time.current.since(2.days))
-      create(:task, title: 'りんごちゃん', state: 'in_progress', deadline: Time.current.since(4.days))
-      create(:task, title: 'パイナップル', state: 'done', deadline: Time.current.since(1.day))
-
-      task_search_form = build(:task_search_form, state: 'not_started', sort_by: 'deadline')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq 'メロン'
-      expect(result[1].title).to eq 'スイカ'
-      expect(result[2].title).to eq '青りんご'
-    end
-
-    it 'ステータス検索の結果を優先順位が高い順にソートできる' do
-      create(:task, title: '青りんご', state: 'in_progress', priority: 'low')
-      create(:task, title: 'スイカ', state: 'not_started', priority: 'mid')
-      create(:task, title: '毒りんご', state: 'in_progress', priority: 'high')
-      create(:task, title: 'りんごちゃん', state: 'in_progress', priority: 'mid')
-      create(:task, title: 'パイナップル', state: 'done', priority: 'high')
-
-      task_search_form = build(:task_search_form, state: 'in_progress', sort_by: 'high')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq '毒りんご'
-      expect(result[1].title).to eq 'りんごちゃん'
-      expect(result[2].title).to eq '青りんご'
-    end
-
-    it 'ステータス検索の結果を優先順位が低い順にソートできる' do
-      create(:task, title: '青りんご', state: 'in_progress', priority: 'low')
-      create(:task, title: 'スイカ', state: 'not_started', priority: 'mid')
-      create(:task, title: '毒りんご', state: 'in_progress', priority: 'high')
-      create(:task, title: 'りんごちゃん', state: 'in_progress', priority: 'mid')
-      create(:task, title: 'パイナップル', state: 'done', priority: 'high')
-
-      task_search_form = build(:task_search_form, state: 'in_progress', sort_by: 'low')
-      result = task_search_form.search
-      expect(result.count).to be 3
-      expect(result[0].title).to eq '青りんご'
-      expect(result[1].title).to eq 'りんごちゃん'
-      expect(result[2].title).to eq '毒りんご'
+      it 'キーワード検索の結果を締切が近い順にソートできる' do
+        task1 = create(:task, :due_tomorrow_task, title: '青りんご', user: user)
+        task2 = create(:task, :due_two_days_after_tomorrow_task, title: '毒りんご', user: user)
+        task3 = create(:task, :due_day_after_tomorrow_task, title: 'りんごちゃん', user: user)
+        task4 = create(:task, title: 'バナナ', user: user)
+        search_form = build(:task_search_form, keyword: 'りんご', sort_by: 'deadline')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 4
+        expect(result.count).to be 3
+        result.each do |task|
+          expect(task.title).to match(/りんご/)
+        end
+        expect(result[0]).to eq task1
+        expect(result[1]).to eq task3
+        expect(result[2]).to eq task2
+      end
+  
+      it 'キーワード検索の結果を優先順位が高い順にソートできる' do
+        task1 = create(:task, title: '青りんご', user: user, priority: 'mid')
+        task2 = create(:task, title: '毒りんご', user: user, priority: 'high')
+        task3 = create(:task, title: 'りんごちゃん', user: user, priority: 'low')
+        task4 = create(:task, title: 'バナナ', user: user)
+  
+        search_form = build(:task_search_form, keyword: 'りんご', sort_by: 'high')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 4
+        expect(result.count).to be 3
+        expect(result[0]).to eq task2
+        expect(result[1]).to eq task1
+        expect(result[2]).to eq task3
+      end
+  
+      it 'キーワード検索の結果を優先順位が低い順にソートできる' do
+        task1 = create(:task, title: '青りんご', user: user, priority: 'mid')
+        task2 = create(:task, title: '毒りんご', user: user, priority: 'high')
+        task3 = create(:task, title: 'りんごちゃん', user: user, priority: 'low')
+        task4 = create(:task, title: 'バナナ', user: user)
+  
+        search_form = build(:task_search_form, keyword: 'りんご', sort_by: 'low')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 4
+        expect(result.count).to be 3
+        expect(result[0]).to eq task3
+        expect(result[1]).to eq task1
+        expect(result[2]).to eq task2
+      end
+  
+      it 'ステータス検索の結果を締切が近い順にソートできる' do
+        task1 = create(:task, :due_tomorrow_task, user: user, state: 'in_progress')
+        task2 = create(:task, :due_day_after_tomorrow_task, user: user, state: 'not_started')
+        task3 = create(:task, :due_two_days_after_tomorrow_task, user: user, state: 'in_progress')
+        search_form = build(:task_search_form, state: 'in_progress', sort_by: 'deadline')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 3
+        expect(result.count).to be 2
+        expect(result[0]).to eq task1
+        expect(result[1]).to eq task3
+      end
+  
+      it 'ステータス検索の結果を優先順位が高い順にソートできる' do
+        task1 = create(:task, user: user, state: 'in_progress', priority: 'low')
+        task2 = create(:task, user: user, state: 'done', priority: 'high')
+        task3 = create(:task, user: user, state: 'in_progress', priority: 'mid')
+  
+        search_form = build(:task_search_form, state: 'in_progress', sort_by: 'high')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 3
+        expect(result.count).to be 2
+        expect(result[0]).to eq task3
+        expect(result[1]).to eq task1
+      end
+  
+      it 'ステータス検索の結果を優先順位が低い順にソートできる' do
+        task1 = create(:task, user: user, state: 'in_progress', priority: 'low')
+        task2 = create(:task, user: user, state: 'done', priority: 'high')
+        task3 = create(:task, user: user, state: 'in_progress', priority: 'mid')
+  
+        search_form = build(:task_search_form, state: 'in_progress', sort_by: 'low')
+        result = search_form.search(user)
+  
+        expect(user.tasks.count).to be 3
+        expect(result.count).to be 2
+        expect(result[0]).to eq task1
+        expect(result[1]).to eq task3
+      end
     end
   end
 end

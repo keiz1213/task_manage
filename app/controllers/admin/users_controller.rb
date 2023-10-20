349221a11eb3434a68b308787b2b42f41da1cba6
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-20 20:57:07 +0900

    ユーザー登録できるようにした

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,29 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
+  def index
+    @user = User.new
+    @users = User.all
   end
 
   def show
   end
 
-  def index
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "ユーザー: #{@user.name}を登録しました"
+      redirect_to admin_users_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

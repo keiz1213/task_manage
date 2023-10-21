f6858b489ce81bf355ef97c8aff0da6f48df6582
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-21 21:27:12 +0900

    seedsをadmin対応

--- db/seeds/production/seeds.rb
+++ db/seeds/production/seeds.rb
@@ -2,11 +2,5 @@ master_user = User.find_or_create_by!(email: 'master@example.com') do |user|
   user.name = 'master'
   user.password = 'password'
   user.password_confirmation = 'password'
-end
-
-tasks_without_user = Task.where(user_id: nil)
-if tasks_without_user.any?
-  tasks_without_user.find_each do |task|
-    task.update!(user_id: master_user.id)
-  end
+  user.admin = true
 end

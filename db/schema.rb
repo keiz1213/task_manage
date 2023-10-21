be80333bffddaf39cb5b540ff6134e4988cd11c5
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-21 21:23:37 +0900

    userにadminフラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_121916) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_121437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_121916) do
     t.datetime "updated_at", null: false
     t.string "password_digest", null: false
     t.string "remember_digest"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

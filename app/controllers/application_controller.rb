6931b6e26999a86f4f7996711c13dc5a22c56e96
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-23 01:09:15 +0900

    ユーザの登録済みタグの表示方法を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :set_search_form
-  before_action :set_tag_names
   before_action :logged_in_user
 
   def set_search_form
     @search_form = TaskSearchForm.new(search_params)
-  end
-
-  def set_tag_names
-    tags = Tag.joins(taggings: { task: :user }).where(users: { id: current_user.id })
-    @tag_names = tags.map(&:name).uniq
   end
 
   def logged_in_user
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :tasks, through: :taggings
 
+  scope :user_tags, ->(user) { joins(taggings: { task: :user }).where(users: { id: user.id }) }
+
   def self.build_tag_list(jointed_tag_names)
     jointed_tag_names.split(',').map(&:strip).uniq
   end

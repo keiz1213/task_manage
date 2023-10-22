efa59fd42b76000964896d1998372b7f0656e218
keiz1213 <fukuoka00tsc@gmail.com>
2023-10-22 21:36:03 +0900

    Tagのモデルスペック追加

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Tag do
+  describe 'build_tag_list' do
+    it 'カンマで区切られた文字列を配列にして返す' do
+      jointed_tag_names = 'foo,bar,baz'
+      tag_list = described_class.build_tag_list(jointed_tag_names)
+      expect(tag_list.count).to be 3
+      tag_list.each do |tag_name|
+        expect(tag_name).to match(/foo|bar|baz/)
+      end
+    end
+  end
 end

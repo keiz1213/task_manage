bbd4aec3f77acf706fbc072fd5dd6919502fb5af
keiz1213 <fukuoka00tsc@gmail.com>
2023-12-19 01:11:05 +0900

    本番用seedsをgit管理から削除

--- db/seeds/production/seeds.rb
+++
@@ -1,6 +0,0 @@
-User.find_or_create_by!(email: 'master@example.com') do |user|
-  user.name = 'master'
-  user.password = 'password'
-  user.password_confirmation = 'password'
-  user.admin = true
-end
